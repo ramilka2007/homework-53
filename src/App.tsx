bccd36333e28626ba77a0689d2df27b945a150f9
ramilka <lilya1976.03.03@gmail.com>
2024-05-24 13:32:20 +0600

    Сделал внешний вид задания, а также добавил кнопку удаления заданий.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css'
+import {useState} from "react";
+import Task from "./Task/Task";
 
 const App = () => {
+    const [tasks, setTasks] = useState([
+        {taskName: 'Do homework', id: 'd1'},
+        {taskName:'Buy milk', id: 'b1'},
+        {taskName:'Walk with dog', id: 'w1'},
+    ]);
+
+    const removeTask = (id: string) => {
+        setTasks((prevTasks) => {
+            return prevTasks.filter((task) => task.id !== id);
+        });
+    };
+
   return (
-    <>
-    </>
+      <div className="app">
+          <form>
+              <input type="text"/>
+              <button type={"submit"}>Add</button>
+          </form>
+          {tasks.map((task) => (
+              <Task key={task.id}
+                    taskName={task.taskName}
+                    removeTask={() => removeTask(task.id)} />
+          ))}
+      </div>
   )
 };
 
